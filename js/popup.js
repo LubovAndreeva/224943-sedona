6b33460ae97e94c257c8b75514fe19531621a390
Lubov <BananaMilk@ya.ru>
2016-09-06 11:54:15 +0300

    правки 8

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 var link = document.querySelector(".btn-search");
-var popup = document.querySelector(".all-form");
+var popup = document.querySelector(".box-search");
 
 link.addEventListener("click", function(event) {
 event.preventDefault();
-popup.classList.toggle("all-form-show");
+popup.classList.toggle("box-search-show");
 });
